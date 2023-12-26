9df8671c5bfa67a3982af91d2d0c3079cb99ae54
ณัฐพร นาคพงษ์ <Missterdonat33@gmail.com>
2023-12-26 16:30:01 +0700

    feat:challenge fetching

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,15 +4,21 @@ import axios from "axios";
 
 function App() {
   const [listProduct, setListProduct] = useState([]);
+  const [status, setStatus] = useState("Loading");
 
   useEffect(() => {
     getListProduct();
   }, []);
 
   const getListProduct = async () => {
-    const result = await axios.get("http://localhost:4001/products");
-    console.log(result);
-    setListProduct(result.data.data);
+    try {
+      const result = await axios.get("http://localhost:4001/products");
+      console.log(result);
+      setListProduct(result.data.data);
+      setStatus("complete");
+    } catch {
+      setStatus("failed");
+    }   
   };
 
   const deleteProduct = async (productId) => {
@@ -20,38 +26,47 @@ function App() {
       .delete(`http://localhost:4001/products/${productId}`)
       .then(getListProduct);
   };
-
-  return (
-    <div className="App">
-      <div className="app-wrapper">
-        <h1 className="app-title">Products</h1>
-      </div>
-      <div className="product-list">
-        {listProduct.map((item, index) => {
-          return (
-            <div key={index} className="product">
-              <div className="product-preview">
-                <img
-                  src={item.image}
-                  alt="some product"
-                  width="350"
-                  height="350"
-                />
+  if (status === "Loading") {
+    return  <span>"Loading..."</span>;
+  } else if (status === "failed") {
+    return <span>"Fetching Error..."</span>;
+  }
+    return (
+      <div className="App">
+        <div className="app-wrapper">
+          <h1 className="app-title">Products</h1>
+        </div>
+        <div className="product-list">
+          {listProduct.map((item, index) => {
+            return (
+              <div key={index} className="product">
+                <div className="product-preview">
+                  <img
+                    src={item.image}
+                    alt="some product"
+                    width="350"
+                    height="350"
+                  />
+                </div>
+                <div className="product-detail">
+                  <h1>Product name: {item.name}</h1>
+                  <h2>Product price: {item.price} Baht</h2>
+                  <p>Product description: {item.description}</p>
+                </div>
+                <button
+                  className="delete-button"
+                  onClick={() => {
+                    deleteProduct(item.id);
+                  }}
+                >
+                  x
+                </button>
               </div>
-              <div className="product-detail">
-                <h1>Product name: {item.name}</h1>
-                <h2>Product price: {item.price} Baht</h2>
-                <p>Product description: {item.description}</p>
-              </div>
-              <button className="delete-button" onClick={() => {
-                deleteProduct(item.id);
-              }}>x</button>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
-    </div>
-  );
+    );
 }
 
 export default App;
